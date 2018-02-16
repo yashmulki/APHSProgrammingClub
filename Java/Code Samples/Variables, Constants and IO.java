fcbf202564fb6eedb69bbd37bdf5793ebf2c8aea
Yashvardhan Mulki <yash.mulki@hotmail.com>
2018-02-16 08:39:52 -0500

    Added part of first lesson as a plain.java file

---
+++ Java/Code Samples/Variables, Constants and IO.java
@@ -0,0 +1,26 @@
+class Main {
+
+  public static void main(String[] args) {
+
+      // This is a variable. It lets programmers store data during the execution of the program
+      int myFirstVariable = 0;
+
+      // Above, myFirstVariable is an Int, or an Integer. It only stores whole numbers
+      // Below are some other data types.
+
+      // Doubles and floats can store decimal numbers
+      Double myFirstDouble = 5.6;
+      Float myFirstFloat = 7.2;
+
+      // Strings Store Text
+      String myFirstString = "Hello World";
+
+      // Chars store individual characters in text
+      char myFirstChar = 'e';
+
+      // You can also change variables
+      myFirstVariable = 5;
+
+  }
+
+}
