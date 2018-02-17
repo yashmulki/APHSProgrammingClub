3b8ae99c3cea2c348e5f893bbcaf172bdbb73fe7
Yashvardhan Mulki <yash.mulki@hotmail.com>
2018-02-17 14:14:19 -0500

    Added a bit of the control flow lesson

---
+++ Java/BasicProgrammingExamples/src/ControlFlowAndLoops.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ControlFlowAndLoops {
+
+	public static void main(String[] args) {
+		
+		// Let's say that we take in a user input
+		Scanner myScanner = new Scanner(System.in);
+		int input = myScanner.nextInt();
+		
+		// And we want to proceed based on the input. How do we do this?
+		// Introducing the if statement
+		
+		if (input > 5) {
+			System.out.println("Input is greater than 5");
+		}
+		
+		
+		
+	}
+	
+}
